8c7af04705cb02f70fe5320a60747fe60ad059a1
Анна <plotnikovanana@gmail.com>
2026-02-23 16:09:12 +0300

    Коммит 8: добавление группы и даты

--- server/Program.cs
+++ server/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Лабораторная работа №4 ===");
+            Console.WriteLine("Приветствую в программе!");
+            
+            // ФИО (уже должно быть)
+            Console.WriteLine("ФИО: Леонтьев Денис Александрович");
+            
+            // ГРУППА (добавьте это)
+            Console.WriteLine("Группа: ИС-21");
+            
+            // ДАТА (добавьте это)
+            Console.WriteLine($"Текущая дата и время: {DateTime.Now}");
+        }
+    }
+}
